52f51d341d2cac192a798ba3283d11419a9a9349
maratkanov-a <a.s.maratkanov@gmail.com>
2017-10-29 20:42:06 +0300

    добавил значения в allowed host

--- back/core/settings.py
+++ back/core/settings.py
@@ -117,7 +117,8 @@ AUTH_PASSWORD_VALIDATORS = [
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
-    'localhost:3030',
+    'localhost:3000',
+    'localhost:3001',
 )
 
 LANGUAGE_CODE = 'en-us'
